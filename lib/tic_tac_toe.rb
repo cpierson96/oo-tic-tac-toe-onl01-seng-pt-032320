77a8419c0f3616675c1cef475aa424270c5ede92
Claudia Pierson <c172@umbc.edu>
2020-04-26 22:38:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,11 +49,12 @@ class TicTacToe
 
   def turn_count
     turn = 0
+    @board.each do |index|
     if index == "X" || index == "O"
     turn += 1
   end
 end
-  return turn 
+  return turn
 end
   def turn_count
   end
