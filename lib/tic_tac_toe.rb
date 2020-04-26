3c4284948987b97ecb6b24e8e2ba74af3e4f1f87
Claudia Pierson <c172@umbc.edu>
2020-04-26 21:30:50 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,73 @@
+require 'pry'
+class TicTacToe
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+  ]
+
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(string)
+     string.to_i - 1
+   end
+
+
+  def move(index, token="X")
+    @board[index] = token
+
+  end
+
+  def position_taken?(index)
+    @board[index] != " "
+  end
+
+  def valid_move?
+
+  end
+
+  def turn
+
+  end
+
+  def turn_count
+
+  end
+
+  def current_player
+  end
+
+  def won
+  end
+
+  def full
+
+  end
+
+  def draw
+  end
+
+  def over
+  end
+
+  def winner
+  end
+
+  def play
+  end
+end
