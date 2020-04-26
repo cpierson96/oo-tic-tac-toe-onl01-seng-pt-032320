e9cfe3894f5a44024d51163ff12878fedc5d7038
Claudia Pierson <c172@umbc.edu>
2020-04-26 21:33:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,19 +1,13 @@
-require 'pry'
 class TicTacToe
-  WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [6, 4, 2]
-  ]
-
-  def initialize(board = nil)
-    @board = board || Array.new(9, " ")
+  def initialize
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
+
+  WIN_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ]
 
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
@@ -23,51 +17,10 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  def input_to_index(string)
-     string.to_i - 1
-   end
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
 
-
-  def move(index, token="X")
+  def move(index, token)
     @board[index] = token
-
   end
-
-  def position_taken?(index)
-    @board[index] != " "
-  end
-
-  def valid_move?
-
-  end
-
-  def turn
-
-  end
-
-  def turn_count
-
-  end
-
-  def current_player
-  end
-
-  def won
-  end
-
-  def full
-
-  end
-
-  def draw
-  end
-
-  def over
-  end
-
-  def winner
-  end
-
-  def play
-  end
-end
